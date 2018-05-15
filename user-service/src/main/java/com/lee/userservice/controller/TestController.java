73ccf7636f659ccacd198f9a10856af375620856
Pwenlee <lipan@xinyunlian.com>
2018-05-15 11:15:13 +0800

    add refreshScope annotation

--- user-service/src/main/java/com/lee/userservice/controller/TestController.java
+++ user-service/src/main/java/com/lee/userservice/controller/TestController.java
@@ -1,13 +1,13 @@
 package com.lee.userservice.controller;
 
-import com.lee.userservice.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope   //这个注解用于动态刷新配置
 public class TestController {
 
     @Value("${server.port}")
